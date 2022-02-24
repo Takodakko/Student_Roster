13700e425af8a6dc3c8055688ca81b5d2099b016
Monchichi <monchichi@Monchichis-Air.lan>
2022-02-24 13:42:55 -0500

    cleaning up unneeded files

--- src/StudentProfile.jsx
+++ src/StudentProfile.jsx
@@ -1,10 +1,12 @@
+import './StudentProfile.css';
+
 function StudentProfile(props) {
   const interests = props.likes.map((like) => {
     return <li key={props.name + like}>{like}</li>
   })
   return (
     <div>
-      <div><strong>Name:</strong> {props.name}</div>
+      <div className="name"><strong>Name:</strong> {props.name}</div>
       <img src={props.pic} height="120px" width="120px"></img>
       <div><strong>Age:</strong> {props.age}</div>
       <ul><strong>Interests:</strong> {interests}</ul>
