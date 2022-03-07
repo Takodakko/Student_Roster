47342fce7773b281cef8abb941585f392f922ad7
Monchichi <monchichi@Monchichis-Air.lan>
2022-03-07 12:26:09 -0500

    added reordering functionality to stat chart

--- src/StatApp.jsx
+++ src/StatApp.jsx
@@ -9,10 +9,28 @@ function StatApp() {
     return (data.json())})
   .then((data) => {
       return setUnitData(data.unitlist)})}, []);
+  
+  const [specialOrder, setSpecialOrder] = useState(false);
 
+  function toggleOrder(property) {
+    if (!specialOrder) {
+      const unitCopy = [...unitData].sort((a, b) => {
+        return (a[property] < b[property]) ? -1 : 1;
+      });
+      setUnitData(unitCopy);
+      setSpecialOrder(!specialOrder)
+    } else {
+      const unitCopy = [...unitData].sort((a, b) => {
+        return (a[property] > b[property]) ? -1 : 1;
+      });
+      setUnitData(unitCopy);
+      setSpecialOrder(!specialOrder);
+    }
+  }
+  
   return (
     <div>
-      <table id="unitgrid"> <tr id="trheader"><th>Picture</th><th>Name</th><th>Base Class</th><th>Base Level</th><th>Base HP</th><th>Base Str</th><th>Base Mag</th><th>Base Dex</th><th>Base Spd</th><th>Base Lck</th><th>Base Def</th><th>Base Res</th><th>Base Cha</th><th>HP Growth</th><th>Str Growth</th><th>Mag Growth</th><th>Dex Growth</th><th>Spd Growth</th><th>Lck Growth</th><th>Def Growth</th><th>Res Growth</th><th>Cha Growth</th></tr> {unitData.map((unit) => {
+      <table id="unitgrid"> <tr id="trheader"><th><button onClick={() => toggleOrder('id')}>Picture</button></th><th><button onClick={() => toggleOrder('name')}>Name</button></th><th><button onClick={() => toggleOrder('startingClass')}>Base Class</button></th><th><button onClick={() => toggleOrder('startingLevel')}>Base Level</button></th><th><button onClick={() => toggleOrder('startingHP')}>Base HP</button></th><th><button onClick={() => toggleOrder('startingStr')}>Base Str</button></th><th><button onClick={() => toggleOrder('startingMag')}>Base Mag</button></th><th><button onClick={() => toggleOrder('startingDex')}>Base Dex</button></th><th><button onClick={() => toggleOrder('startingSpd')}>Base Spd</button></th><th><button onClick={() => toggleOrder('startingLck')}>Base Lck</button></th><th><button onClick={() => toggleOrder('startingDef')}>Base Def</button></th><th><button onClick={() => toggleOrder('startingRes')}>Base Res</button></th><th><button onClick={() => toggleOrder('startingCha')}>Base Cha</button></th><th><button onClick={() => toggleOrder('growthHP')}>HP Growth</button></th><th><button onClick={() => toggleOrder('growthStr')}>Str Growth</button></th><th><button onClick={() => toggleOrder('growthMag')}>Mag Growth</button></th><th><button onClick={() => toggleOrder('growthDex')}>Dex Growth</button></th><th><button onClick={() => toggleOrder('growthSpd')}>Spd Growth</button></th><th><button onClick={() => toggleOrder('growthLck')}>Lck Growth</button></th><th><button onClick={() => toggleOrder('growthDef')}>Def Growth</button></th><th><button onClick={() => toggleOrder('growthRes')}>Res Growth</button></th><th><button onClick={() => toggleOrder('growthCha')}>Cha Growth</button></th></tr> {unitData.map((unit) => {
         return <UnitProfile key={unit.id} id={unit.id} name={unit.name} face={unit.face} startinglevel={unit.startingLevel} startingclass={unit.startingClass} startinghp={unit.startingHP} startingstr={unit.startingStr} startingmag={unit.startingMag} startingdex={unit.startingDex} startingspd={unit.startingSpd} startinglck={unit.startingLck} startingdef={unit.startingDef} startingres={unit.startingRes} startingcha={unit.startingCha} growthhp={unit.growthHP} growthstr={unit.growthStr} growthmag={unit.growthMag} growthdex={unit.growthDex} growthspd={unit.growthSpd} growthlck={unit.growthLck} growthdef={unit.growthDef} growthres={unit.growthRes} growthcha={unit.growthCha} house={unit.house}/>
       })}
       
